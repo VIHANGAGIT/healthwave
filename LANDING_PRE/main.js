fb736b57877e09e51096d4d27efb93d567aa6f2c
hemaperera <123574561+hemaperera@users.noreply.github.com>
2024-04-12 20:53:50 +0530

    Add files via upload

---
+++ LANDING_PRE/main.js
@@ -0,0 +1,13 @@
+//swiper
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".swiper-container", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        centeredSlides: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
+
