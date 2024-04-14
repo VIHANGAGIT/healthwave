4cf82d2c5317e51d30996bff7a67902029b9bcb7
minura <minurajay2000@gmail.com>
2024-04-14 12:54:29 +0530

    change

--- app/views/doctor/addprescription.php
+++ app/views/doctor/addprescription.php
@@ -16,8 +16,9 @@
     <link flex href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <script src="<?php echo URLROOT;?>/js/light_mode.js" defer></script>
+    <script src="<?php echo URLROOT;?>/js/form.js" defer></script>
 
-    <link rel="js" href="form.js">
+
     <!-- Fontawesome CDN Link For Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
   </head>
