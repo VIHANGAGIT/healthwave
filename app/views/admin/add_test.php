4d4fdd2104a1680d8b55a30aa75aaee6c480eaea
minura <minurajay2000@gmail.com>
2024-04-21 09:11:41 +0530

    add_test changes

--- app/views/admin/add_test.php
+++ app/views/admin/add_test.php
@@ -14,59 +14,45 @@
    <title>Hospital Regisration</title>
 </head>
 <body>
-    <div class="container-signup" style="height: 720px;" >
-        <header>Add Test</header>
+<div class="container-signup" style="height: 720px;">
+    <header>Add Test</header>
 
-        <form action="<?php //echo URLROOT; ?>/users/register_doctor" method="POST" style="height: 600px;">
-            <div class="form first" >
-                <div class="details personal">
-                    <span class="title">Test Details</span>
+    <form action="<?php //echo URLROOT; ?>/users/register_doctor" method="POST" style="height: 600px;">
+        <div class="form first">
+            <div class="details personal">
+                <span class="title">Test Details</span>
 
-                    <div class="fields">
-                        <div class="input-field">
-                            <label>Test Name*</label>
-                            <input type="text" placeholder="Enter test name" name="testname" value="<?php //echo $data['F_name'] ?>" required>
-                        </div>
+                <div class="fields">
+                    <div class="input-field" style="width: calc(50% - 10px); margin-right: 10px;">
+                        <label>Test Name*</label>
+                        <input type="text" placeholder="Enter test name" name="testname" value="<?php //echo $data['F_name'] ?>" required>
+                    </div>
 
-                        
-
-                        <div class="input-field">
-                            <label>Test Type*</label>
-
-                            <select name="region" required>
-                            <option selected value=" <?php //echo $data['Gender'] ?>" > <?php //echo ($data['Gender'] == '') ? 'Select test type' : $data['Gender'] ?></option>
-                                <option>Test Type 1</option>
-                                <option>Test Type 2</option>
-                                <option>Test Type 3</option>
-                                <option>Test Type 4</option>
-                                
-
-                            </select>
-                        </div>
-
-                        
-
-                       
-                       
+                    <div class="input-field" style="width: calc(50% - 10px);">
+                        <label>Test Type*</label>
+                        <select name="region" required>
+                            <option selected value="<?php //echo $data['Gender'] ?>"><?php //echo ($data['Gender'] == '') ? 'Select test type' : $data['Gender'] ?></option>
+                            <option>Test Type 1</option>
+                            <option>Test Type 2</option>
+                            <option>Test Type 3</option>
+                            <option>Test Type 4</option>
+                        </select>
                     </div>
                 </div>
-
-                    <div class="buttons">
-                        <button>
-                            <span class="btnText">Clear</span>
-                        </button>
-                        
-                        <button class="sumbit">
-                            <span class="btnText">Add</span>
-                        </button>
-                    </div>
-                </div>
-                   
-                </div>
-                
             </div>
-        </form>
+
+            <div class="buttons">
+                <button style="width: calc(50% - 10px);">
+                    <span class="btnText">Clear</span>
+                </button>
+                <button class="submit" style="width: calc(50% - 10px);  margin-left: 15px;">
+                    <span class="btnText">Add</span>
+                </button>
+            </div>
+        </div>
+    </form>
 </div>
+
 
     <script src="<?php echo URLROOT; ?>/js/signup.js"></script>
 </body>
