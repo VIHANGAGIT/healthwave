431bc400687581b5d91d47128ed9c48c34e4bc19
minura <minurajay2000@gmail.com>
2024-04-25 22:41:28 +0530

    edit_appointments UI changes

--- app/views/admin/edit_appointments.php
+++ app/views/admin/edit_appointments.php
@@ -5,91 +5,95 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <!----======== CSS ======== -->
+    <!-- CSS -->
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
-     
-    <!----===== Iconscout CSS ===== -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
-   <title>Hospital Update</title>
+   <title>Appointments Update</title>
 </head>
 <body>
-    <div class="container-signup" style="height: 720px;" >
-        <header>Hospital Update</header>
+    <div class="container-signup" style="height: 620px; overflow: hidden;">
+        <header>Appointments Update</header>
 
         <form action="<?php echo URLROOT; ?>/admin/edit_appointments" method="POST" style="height: 600px;">
-            <div class="form first" >
+            <div class="form first">
                 <div class="details personal">
-                    <span class="title">Hospital Details</span>
+                    <span class="title">Appointment Details</span>
 
-                    <input type="hidden" name="hid" value="<?php //echo $data['H_ID'] ?>">
+                    <!-- Hidden input field for data -->
+                    <input type="hidden" name="" value="<?php //echo $data['H_ID'] ?>">
 
                     <div class="fields">
                         <div class="input-field">
-                            <label>Hospital Name*</label>
-                            <input type="text" placeholder="Enter hospital name" name="hname" value="<?php //echo $data['H_name'] ?>" required>
+                            <label>Reservation ID*</label>
+                            <input type="text" placeholder="" name="" value="<?php //echo $data['H_name'] ?>" required disabled>
                         </div>
 
                         <div class="input-field">
-                            <label>Address*</label>
-                            <input type="text" placeholder="Enter hospital adress" name="haddress" value="<?php //echo $data['H_address'] ?>" required>
+                            <label>Patient Name*</label>
+                            <input type="text" placeholder="" name="" value="<?php //echo $data['H_address'] ?>" required disabled>
                         </div>
 
                         <div class="input-field">
-                            <label>Region*</label>
+                            <label>NIC*</label>
+                            <input type="text" placeholder="" name="" value="<?php //echo $data['H_address'] ?>" required disabled>
+                        </div>
 
-                            <select name="region" required>
-                            <option selected value="<?php //echo $data['Region'] ?>" > <?php //echo ($data['Region'] == '') ? 'Select region' : $data['Region'] ?></option>
-                                <option>Colombo</option>
-                                <option>Kandy</option>
-                                <option>Galle</option>
-                                <option>Matara</option>
-                                <option>Kurunegala</option>
-                                <option>Badulla</option>
-                                <option>Anuradhapura</option>
-                                <option>Polonnaruwa</option>
-                                <option>Trincomalee</option>
-                                <option>Jaffna</option>
-                                <option>Other</option>
-
-                            </select>
+                        <!-- Radio buttons for Date -->
+                        <div class="input-field">
+                            <label>Date*</label>
+                            <div class="radio-container">
+                                <label><input type="radio" name="appointment_date" value="2024-04-25" required> April 25, 2024</label>
+                                
+                                <!-- Add more radio buttons for other dates as needed -->
+                            </div>
                         </div>
 
                         <div class="input-field">
-                            <label>Hospital Charge*</label>
-                            <input type="text" placeholder="Enter hospital charge" name="hcharge" value="<?php //echo $data['H_charge'] ?>" required>
+                            <label>Date</label>
+                            <div class="radio-container">
+                            <label><input type="radio" name="appointment_date" value="2024-04-25" required> April 25, 2024</label>
+                              
+                            </div>
                         </div>
 
                         <div class="input-field">
-                            <label>Contact Number*</label>
-                            <input type="text" placeholder="Enter hospital contact number" name="cnum" value="<?php //echo $data['C_num'] ?>" required>
-                            <span class="err-msg"><?php //echo $data['C_num_err'] ?></span>
-
+                            <label>Date*</label>
+                            <div class="radio-container">
+                            <label><input type="radio" name="appointment_date" value="2024-04-25" required> April 25, 2024</label>
+                               
+                            </div>
                         </div>
-                        <div class="input-field">
-                            <label></label>
-                            <input type="hidden" placeholder="" name="" value="<?php //echo $data['C_num'] ?>" required>
 
+                        <div class="input-field">
+                            <label>Appointment Number*</label>
+                            <input type="text" placeholder="Enter appointment number" name="" value="<?php //echo $data['C_num'] ?>" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>Start Time*</label>
+                            <input type="time" placeholder="Enter appointment number" name="" value="<?php //echo $data['C_num'] ?>" required>
+                        </div>
+
+                        <div class="input-field">
+                            <label>End Time*</label>
+                            <input type="time" placeholder="" name="" value="<?php //echo $data['C_num'] ?>" required>
                         </div>
                     </div>
                 </div>
 
-                    <div class="buttons">
-                        <button type="button" onclick="window.history.back()" >
-                            <span class="btnText">Back</span>
-                        </button>
+                <div class="buttons">
+                    <button type="button" onclick="window.history.back()">
+                        <span class="btnText">Back</span>
+                    </button>
                         
-                        <button class="sumbit">
-                            <span class="btnText">Sumbit</span>
-                        </button>
-                    </div>
+                    <button class="submit">
+                        <span class="btnText">Submit</span>
+                    </button>
                 </div>
-                   
-                </div>
-                
             </div>
         </form>
-</div>
+    </div>
 
     <script src="<?php echo URLROOT; ?>/js/signup.js"></script>
 </body>
